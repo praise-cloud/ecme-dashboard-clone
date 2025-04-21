3668e83cbd948a04e81ec71cd3a8b59c2fe81145
praise-cloud <georgewillpraise1@gmail.com>
2025-04-21 11:19:01 +0100

    modified the dashboard route to be exclused

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   description: "Next.js Management System",
 };
 
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
